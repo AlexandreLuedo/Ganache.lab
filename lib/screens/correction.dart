3efdb9891216ef8e13e361b432f1093785d334a9
Alexandre Luedo <Alexandre.luedo@pm.me>
2025-08-16 16:52:32 +0200

    feat: Create initial Correction screen
    This commit introduces the `Correction` screen.
    
    - Added a new file `lib/screens/correction.dart`.
    - The `Correction` widget is a `StatelessWidget` that displays a `Scaffold` with an `AppBar` titled "Ganache chocolat Callebaut" and a `Placeholder` as its body.

---
+++ lib/screens/correction.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Correction extends StatelessWidget {
+  const Correction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Ganache chocolat Callebaut")),
+      body: Placeholder(),
+    );
+  }
+}
