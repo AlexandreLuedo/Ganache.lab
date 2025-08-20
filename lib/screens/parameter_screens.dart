70c67196219eeef7d6c8e9eb1519c39be85f7914
Alexandre Luedo <Alexandre.luedo@pm.me>
2025-08-20 11:24:05 +0200

    Refactor: Add playbook.dart and update TODO in parameter_screens.dart
    - Added a new, currently empty, file `lib/models/playbook.dart`.
    - Updated a `TODO` comment in `lib/screens/parameter_screens.dart` to reference Flutter documentation for key-value persistence.

--- lib/screens/parameter_screens.dart
+++ lib/screens/parameter_screens.dart
@@ -9,7 +9,7 @@ class Settings extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Paramètres')),
       // TODO Adding dark theme (SearrchBar class doc)
-      // TODO utiliser un package parameters
+      // TODO https://docs.flutter.dev/cookbook/persistence/key-value
     );
   }
 }
