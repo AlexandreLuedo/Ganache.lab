322d5513e7bc7a7ec89557b9eaaf869fc715c726
Alexandre Luedo <Alexandre.luedo@pm.me>
2025-09-07 19:52:27 +0200

    Refactor: Relocate main.dart and update application display name
    This commit moves `main.dart` from the `lib/utils/` directory to `lib/` and updates the application's display name to "Ganache.lab".
    
    - **File Relocation:**
        - `lib/utils/main.dart` moved to `lib/main.dart`.
        - Updated import path in `test/widget_test.dart` to reflect the new location.
    
    - **Android Manifest:**
        - Changed `android:label` in `android/app/src/main/AndroidManifest.xml` from "ganache_dot_first" to "Ganache.lab".
    
    - **UI Update (Recipes Screen):**
        - Added a `NoRecipeMessage` widget to `lib/screens/recipes_screen.dart`. This widget is displayed when there are no recipes, showing an icon and an informative message.
    
    - **Documentation:**
        - Removed the auto-generated Table of Contents from `assets/documentations/DewPoint.md`.

--- assets/documentations/DewPoint.md
+++ assets/documentations/DewPoint.md
@@ -1,11 +1,2 @@
-  * [ ] 
-# Table of Contents
-
-1.  [Le point de rosé](#org86d9882)
-
-
-
-
-
 # Le point de rosé
 
--- lib/screens/recipes_screen.dart
+++ lib/screens/recipes_screen.dart
@@ -1,6 +1,7 @@
 // Affiche les recettes via la bdd
 import 'package:flutter/material.dart';
 import 'package:ganache_lab/widgets/widgets_exportation_file.dart';
+import 'package:material_symbols_icons/symbols.dart';
 
 class RecipeList extends StatelessWidget {
   const RecipeList({super.key});
@@ -15,6 +16,34 @@ class RecipeList extends StatelessWidget {
           'Recettes' /*,
         style: TextStyle(fontWeight: FontWeight.bold)*/,
         ),
+      ),
+      body: NoRecipeMessage()
+    );
+  }
+}
+
+class NoRecipeMessage extends StatelessWidget {
+  const NoRecipeMessage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          SizedBox(height: 100),
+          Icon(
+            Symbols.menu_book_2,
+            size: 250,
+            color: Colors.grey,
+            weight: 100,
+          ),
+          SizedBox(height: 100),
+          Text(
+            'Aucune recette n\'est calculée pour le moment. \n Vous pouvez en créer une en cliquant sur "Créer sa ganache"',
+            style: TextStyle(color: Colors.grey, fontSize: 25),
+            textAlign: TextAlign.center,
+          ),
+        ],
       ),
     );
   }
