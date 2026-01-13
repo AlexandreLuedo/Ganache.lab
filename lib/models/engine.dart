63408fd57b77dfea17261a1143b39169f2545f2f
Alexandre Luedo <Alexandre.luedo@pm.me>
2026-01-13 19:39:58 +0100

    feat(engine): add code for engine calculation

---
+++ lib/models/engine.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:ganache_lab/widgets/chip_ganache_type.dart';
+
+int whatIsThisType = chocolateTypeVal€ue;
+
+if (chocolateTypeValue == 0) {
+whatIsThisType = privateConfig['dark'];
+} else if (chocolateTypeValue == 1) {
+whatIsThisType = privateConfig['milk'];
+}
