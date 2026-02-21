7c3478bf60e05a05cb987516db666538949d67cd
Alexandre Luedo <alexandre.luedo@pm.me>
2026-02-21 17:45:09 +0100

    fix: good email filling

--- lib/utils/email_helper.dart
+++ lib/utils/email_helper.dart
@@ -2,8 +2,9 @@ import 'package:url_launcher/url_launcher.dart';
 
 void openEmail() async {
   final subject = Uri.encodeComponent('Contact - Application Ganache.lab');
-  final body = Uri.encodeComponent('Bonjour,\nJe vous contacte car...');
-  final Uri emailUri = Uri.parse('mailto:hadrien.ganache@pm.me?subject=$subject&body=$body');
+  final Uri emailUri = Uri.parse(
+    'mailto:contact.ganache@pm.me?subject=$subject',
+  );
 
   if (!await launchUrl(emailUri, mode: LaunchMode.externalApplication)) {
     print('Impossible d\'ouvrir l\'application mail');
