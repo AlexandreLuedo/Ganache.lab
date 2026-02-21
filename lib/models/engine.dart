3a4ae6b8db644a3605cfe0f5aeb82ccc8118153d
Alexandre Luedo <alexandre.luedo@pm.me>
2026-02-21 17:42:21 +0100

    Erease unusued values

--- lib/models/engine.dart
+++ lib/models/engine.dart
@@ -1,10 +1,1 @@
-import 'package:flutter/material.dart';
-import 'package:ganache_lab/widgets/chip_ganache_type.dart';
 
-int whatIsThisType = chocolateTypeValue;
-
-if (chocolateTypeValue == 0) {
-whatIsThisType = privateConfig['dark'];
-} else if (chocolateTypeValue == 1) {
-whatIsThisType = privateConfig['milk'];
-}
