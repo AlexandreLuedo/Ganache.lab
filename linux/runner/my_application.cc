0d41e0dfd635b10174696c15b5c7098848e09b64
Alexandre Luedo <Alexandre.luedo@pm.me>
2026-03-07 16:58:02 +0100

    feat: added names & descriptions for all versions

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "ganache_dot_first");
+    gtk_header_bar_set_title(header_bar, "Ganache.lab");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "ganache_dot_first");
+    gtk_window_set_title(window, "Ganache.lab");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
