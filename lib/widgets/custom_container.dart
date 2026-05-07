3534e00acfa20e841dfe30d68908f9df95a04458
Alexandre Luedo <Alexandre.luedo@pm.me>
2026-05-07 21:33:42 +0200

    ui: add entrance fade-in and slide animation to CustomContainer

--- lib/widgets/custom_container.dart
+++ lib/widgets/custom_container.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
-class CustomContainer extends StatelessWidget {
-  final Color? color; // couleur de fond (optionnelle)
-  final double padding; // padding interne
-  final double margin; // margin externe
-  final double borderRadius; // rayon des coins
-  final double? borderWidth; // épaisseur de la bordure (facultatif)
-  final Color? borderColor; // couleur de la bordure (facultatif)
+class CustomContainer extends StatefulWidget {
+  final Color? color;
+  final double padding;
+  final double margin;
+  final double borderRadius;
+  final double? borderWidth;
+  final Color? borderColor;
   final Widget child;
-  final double? width; // largeur fixe (optionnelle)
-  final double? height; // hauteur fixe (optionnelle)
+  final double? width;
+  final double? height;
+  final bool animate;
 
   const CustomContainer({
     super.key,
@@ -21,31 +22,81 @@ class CustomContainer extends StatelessWidget {
     this.borderColor,
     this.width,
     this.height,
+    this.animate = true,
     required this.child,
   });
 
   @override
+  State<CustomContainer> createState() => _CustomContainerState();
+}
+
+class _CustomContainerState extends State<CustomContainer>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _opacityAnimation;
+  late Animation<Offset> _slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 600),
+    );
+
+    _opacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeOut),
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0, 0.1),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+
+    if (widget.animate) {
+      _controller.forward();
+    } else {
+      _controller.value = 1.0;
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final Color background =
-        color ?? Theme.of(context).colorScheme.surfaceContainer;
+        widget.color ?? Theme.of(context).colorScheme.surfaceContainer;
     final Color resolvedBorderColor =
-        borderColor ?? Theme.of(context).dividerColor;
+        widget.borderColor ?? Theme.of(context).dividerColor;
 
-    return Container(
-      width: width,
-      height: height,
-      margin: EdgeInsets.all(margin),
-      padding: EdgeInsets.all(padding),
-      decoration: BoxDecoration(
-        color: background,
-        borderRadius: BorderRadius.circular(borderRadius),
-        border:
-            borderWidth != null
-                ? Border.all(width: borderWidth!, color: resolvedBorderColor)
-                : null,
+    return FadeTransition(
+      opacity: _opacityAnimation,
+      child: SlideTransition(
+        position: _slideAnimation,
+        child: Container(
+          width: widget.width,
+          height: widget.height,
+          margin: EdgeInsets.all(widget.margin),
+          padding: EdgeInsets.all(widget.padding),
+          decoration: BoxDecoration(
+            color: background,
+            borderRadius: BorderRadius.circular(widget.borderRadius),
+            border:
+                widget.borderWidth != null
+                    ? Border.all(
+                      width: widget.borderWidth!,
+                      color: resolvedBorderColor,
+                    )
+                    : null,
+          ),
+          clipBehavior: Clip.hardEdge,
+          child: widget.child,
+        ),
       ),
-      clipBehavior: Clip.hardEdge,
-      child: child,
     );
   }
 }
